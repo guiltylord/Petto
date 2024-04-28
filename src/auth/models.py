2078f720826523583b72efb174e8a4be13b122f9
Panin-ID <old.mail1337@gmail.com>
2024-04-28 18:06:40 +0300

    Added user field: name, surname.

--- src/auth/models.py
+++ src/auth/models.py
@@ -27,24 +27,12 @@ role = Table(
     Column("permissions", JSON),
 )
 
-# user = Table(
-#     "user",
-#     metadata,
-#     Column("id", Integer, primary_key=True),
-#     Column("username", String, nullable=False),
-#     Column("email", String, nullable=False),
-#     Column("hashed_password", String, nullable=False),
-#     Column("role_id", Integer, ForeignKey(role.c.id)),
-#     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-#     Column("is_active", Boolean, default=True, nullable=False),
-#     Column("is_superuser", Boolean, default=False, nullable=False),
-#     Column("is_verified", Boolean, default=False, nullable=False),
-# )
-
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    surname: Mapped[str] = mapped_column(String, nullable=False)
     email: Mapped[str] = mapped_column(
         String(length=320), unique=True, index=True, nullable=False
     )
