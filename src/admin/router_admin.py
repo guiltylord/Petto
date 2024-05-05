233040429b255d3c4f9e429058ec14a127cbb62e
guiltylord <desxzawq@gmail.com>
2024-05-05 18:24:36 +0300

    delete change name route

--- src/admin/router_admin.py
+++ src/admin/router_admin.py
@@ -35,12 +35,18 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
-
             # Разделяем сообщение на команду и данные
             command, _, message_data = data.partition(":")
+            if message_data == "":
+                await websocket.send_text("Введите Id пользователя")
+                return
+
+            try:
+                user_id = int(message_data)
+            except ValueError:
+                await websocket.send_text("Введите число")
 
             if command == "get_hash_user":
-                user_id = int(message_data)
                 hash_user = await getHashUser(user_id)
                 await websocket.send_text(hash_user)
                 print(hash_user)
@@ -55,12 +61,10 @@ async def websocket_endpoint(websocket: WebSocket):
                 await websocket.send_text(f"Эхо: {message_data}")
 
             elif command == "get_user_data":
-                user_id = int(message_data)  # Передаем user_id как целое число
                 user_info = await getUserInfo(user_id, True)
                 await websocket.send_text(str(user_info))
                 print(user_info)
             elif command == "user_weight":
-                user_id = int(message_data)  # Передаем user_id как целое число
                 user_info = await getUserWeight(user_id)
                 await websocket.send_text(str(user_info))
     except WebSocketDisconnect:
